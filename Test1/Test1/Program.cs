6e1174d00efbcda3c8c2b4a35be09087471ab4cc
TuanAnh <pvtuananh1505@gmail.com>
2024-05-11 17:10:28 +0700

    first commit

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace Test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Dieu Linh");
+        }    
+    }
+}
